3aa970cd7faed5ec320df526d2f95fe2baa50764
KybalionX <nflorez@ubits.co>
2024-08-09 23:36:41 -0500

    Fix: change publicDir in vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/le-festin/',
-  publicDir: '/le-festin/',
+  publicDir: '/le-festin/public/',
   build: {
     outDir: './build',
     emptyOutDir: true,
